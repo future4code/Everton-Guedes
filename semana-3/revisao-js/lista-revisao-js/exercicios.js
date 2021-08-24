d460c89bfc539da1153b32e6dde872646c93c61e
Everton Guedes <evertonguedes1@gmail.com>
2021-08-23 21:40:53 -0300

    resolução lista revisão JavaScript

---
+++ semana-3/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,106 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   const retorno = array.length
+
+      return retorno
+}
+
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    const inverter = array.reverse()  
+
+        return inverter
+}
+
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+    const ordenar = array.sort(compararNumeros) 
+function compararNumeros(a, b) {
+        return a - b;
+      }
+    
+    return ordenar
+    }
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+    const pares = array.filter((valor) => {
+    if(valor % 2 === 0) { 
+        return true
+    } else {
+        return false }
+    })
+    return pares
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
