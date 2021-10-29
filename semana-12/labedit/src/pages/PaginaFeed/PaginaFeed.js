51c1c5b80e27f4df77fb19a9f276316e9bdf4414
Everton Guedes <evertonguedes1@gmail.com>
2021-10-29 00:00:22 -0300

    atualizações labedit

--- semana-12/labedit/src/pages/PaginaFeed/PaginaFeed.js
+++ semana-12/labedit/src/pages/PaginaFeed/PaginaFeed.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 const PaginaFeed = () => {
     return (
         <div>
             <h1>PaginaFeed</h1>
+            <Button variant="contained" color="primary">
+                Era pra ser Laranja
+            </Button>
         </div>
     )
 }
