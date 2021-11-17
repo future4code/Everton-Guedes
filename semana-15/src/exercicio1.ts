32a5874eb8b0c9a08c32c3320970e6cae95ab5d5
Everton Guedes <evertonguedes1@gmail.com>
2021-11-17 20:00:39 -0300

    exercícios aula 42

---
+++ semana-15/src/exercicio1.ts
@@ -0,0 +1,15 @@
+// Exercício 1
+
+// a)
+// Quando ocorre a tentativa de atribuir um número a uma variável declarada string
+// ela não é lida antes mesmo de executar o código.
+
+const MinhaString: string = "Olá"
+
+console.log(MinhaString)
+
+// b)
+
+const meuNumero: number | string = 23
+
+console.log(meuNumero)
