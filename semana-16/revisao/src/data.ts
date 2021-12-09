a993be6d8eadfb2d35c3b9a33fa680c30813c9b1
Everton Guedes <evertonguedes1@gmail.com>
2021-12-09 17:10:16 -0300

    revisando a revisao para fazer o projeto

---
+++ semana-16/revisao/src/data.ts
@@ -0,0 +1,14 @@
+import { movie } from "./types";
+
+export const movies: movie [] = [
+    {
+        id: 1,
+        title: "X-men: O filme",
+        year: 2000
+    }
+    {
+        id: 2,
+        title: "Deadpool",
+        year: 2016
+    }
+]
